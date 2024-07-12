184fee496f2e8a2cd5cb4ea31bd5f8cca6729d4b
ernie <ernie_hs@hotmail.com>
2024-07-12 09:40:26 +0100

    tinker

--- src/test1/core.cljs
+++ src/test1/core.cljs
@@ -1,6 +1,49 @@
-(ns test1.core)
- 
-(js/console.log "ernie")
+(ns test1.core
+  (:require [three :as t]))
+
+(defn get-window-dims
+ "get the dimensions of js/window"
+ []
+ {:width (.-innerWidth js/window) :height (.-innerHeight js/window)})
+
+(defn get-aspect-ratio
+  "get the aspect ratio of js/window"
+  []
+  (let [dims (get-window-dims)]
+    (/ (:width dims) (:height dims))))
+
+;; globals
+
+(def canvas (.querySelector js/document "#grid"))
+(def renderer (t/WebGLRenderer. (js-obj "canvas" canvas) "antialias" true))
+(def camera (t/PerspectiveCamera. 45 (get-aspect-ratio) 0.1 1000))
+(def scene (t/Scene.))
+
+(.setSize renderer (let [d (get-window-dims)]
+                     (:width d) (:height d)))
+
+(set! (.-z (.-position camera)) 5)
+
+;; do something
+
+(def geometry (t/BoxGeometry. 1 1 1))
+(def material (t/MeshBasicMaterial. (js-obj "color" 0x00ff00)))
+(def box (t/Mesh. geometry material))
+
+(.add scene box)
+
+(defn animation
+  "animation loop used by renderer, do and draw stuff here"
+  []
+  (let [y (.-y (.-rotation box))
+        z (.-z (.-rotation box))]
+    (set! (.-y (.-rotation box)) (+ 0.01 y))
+    (set! (.-z (.-rotation box)) (+ 0.01 z))
+    (.render renderer scene camera)))
+
+(.setAnimationLoop renderer animation)
+
+;; shadow stuff
 
 (defn start []
   (js/console.log "start"))
